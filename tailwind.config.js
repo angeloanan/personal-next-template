222f3da9e127210c1ed9276ebef2632a064ae1f5
Christopher Angelo <angelo@angeloanan.xyz>
2021-12-18 08:32:06 +0700

    chore: upgrade to stuff
    * supports yarn berry but still set version to yarn classic
    * includes `interactive` plugin
    * upgrade tailwind 3.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
