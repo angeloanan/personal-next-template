e58c2d6c73e10382e7b1549e2005a296e404fd1f
Christopher Angelo <angelo@angeloanan.xyz>
2022-03-03 21:20:56 +0700

    feat: typecheck sitemap config

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,7 +1,12 @@
+// @ts-check
+
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://angeloanan.xyz',
+
   generateRobotsTxt: true,
   sitemapSize: 7000,
+
   robotsTxtOptions: {
     policies: [
       {
