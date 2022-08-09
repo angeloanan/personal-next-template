d5f4e887b70cac15fc6f021cde4bf9764a5094e4
Christopher Angelo <angelo@angeloanan.xyz>
2022-08-09 23:10:49 +0700

    feat: upgrade deps & actually uses inter as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss/types').Config} */
 module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   variants: {
     extend: {}
